9a524d7c133e0b6bf289c71b4b539c28a5d1d19c
Evandro Calado <evandrocalado07@gmail.com>
2023-04-13 10:29:42 -0300

    feat: create TextInput component with styles, storybbok and tests

---
+++ src/components/TextInput/stories.tsx
@@ -0,0 +1,118 @@
+import { AlternateEmail } from '@styled-icons/material-outlined';
+import TextInput, { TextInputProps } from '.';
+import { Meta, StoryFn } from '@storybook/react';
+
+export default {
+  title: 'TextInput',
+  component: TextInput,
+  args: {
+    label: 'Teste label',
+    name: 'input-name',
+    type: 'text',
+    disabled: false,
+    errorMessage: '',
+  },
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <div style={{ padding: '3.2rem' }}>
+          <Story />
+        </div>
+      );
+    },
+  ],
+  argTypes: {
+    icon: {
+      type: null,
+    },
+    value: {
+      type: {
+        name: 'string',
+      },
+    },
+    type: {
+      control: {
+        type: 'select',
+        options: ['text', 'password', 'email'],
+      },
+    },
+  },
+} as Meta<TextInputProps>;
+
+export const Template: StoryFn<TextInputProps> = (args) => {
+  return (
+    <div>
+      <TextInput {...args} />
+    </div>
+  );
+};
+
+export const OnError: StoryFn<TextInputProps> = (args) => {
+  return (
+    <div>
+      <TextInput {...args} />
+    </div>
+  );
+};
+
+OnError.args = {
+  errorMessage: 'Something went wrong, Sorry!',
+  value: 'Something you typed',
+  icon: <AlternateEmail />,
+};
+
+export const WithText: StoryFn<TextInputProps> = (args) => {
+  return (
+    <div>
+      <TextInput {...args} />
+    </div>
+  );
+};
+
+WithText.args = {
+  value: 'Something you typed',
+};
+
+export const WithIcon: StoryFn<TextInputProps> = (args) => {
+  return (
+    <div>
+      <TextInput {...args} />
+    </div>
+  );
+};
+
+WithIcon.args = {
+  value: 'Something you typed',
+  icon: <AlternateEmail />,
+};
+
+export const Disabled: StoryFn<TextInputProps> = (args) => {
+  return (
+    <div>
+      <TextInput {...args} />
+    </div>
+  );
+};
+
+Disabled.args = {
+  value: 'Something you typed',
+  icon: <AlternateEmail />,
+  disabled: true,
+};
+
+export const TextArea: StoryFn<TextInputProps> = (args) => {
+  return (
+    <div>
+      <TextInput {...args} />
+    </div>
+  );
+};
+
+TextArea.args = {
+  value: 'Something you typed',
+  icon: <AlternateEmail />,
+  as: 'textarea',
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { theme } from '../styles/theme';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import { Open_Sans } from '@next/font/google';
+import { Open_Sans, Montserrat } from '@next/font/google';
 
 const openSans = Open_Sans({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700', '800'],
+});
+
+const montserrat = Montserrat({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700', '800'],
 });
@@ -11,7 +16,7 @@ const openSans = Open_Sans({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <div className={openSans.className}>
+      <div className={`${openSans.className} ${montserrat.className}`}>
         <Component {...pageProps} />
       </div>
     </ThemeProvider>
